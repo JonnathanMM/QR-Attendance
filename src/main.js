70d5c145771f750e58732dbbd45cf3409506e2a5
JonnathanMM <jonnathan.2019110012@metropoli.edu.mx>
2023-06-06 14:28:35 -0700

    Employes views design

--- src/main.js
+++ src/main.js
@@ -8,5 +8,5 @@ import './assets/css/styles.css'
 import router from './router'
 
 const app = createApp(App)
-app.use(router)
+app.use(router) // Instala el enrutador en la aplicación
 app.mount('#app')
